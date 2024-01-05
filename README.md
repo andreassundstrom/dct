bb3b7b611bcc392b9efb43eabdbf4151c66541cf
Andreas Sundström <andreas-sundstrom@hotmail.com>
2024-01-05 20:29:57 +0100

    readme and randomize feature

---
+++ README.md
@@ -0,0 +1,9 @@
+# DICOM Tools (dct)
+
+A simple cli to manipulate dicom files.
+
+To randomize a file simply run:
+
+```sh
+dct randomize --filename 0001.dcm
+```
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
+use dicom::core::chrono::Datelike;
+use dicom::core::value::DicomDate;
 use dicom::dicom_value;
-use dicom::object::{open_file, Tag};
+use dicom::object::{open_file, FileDicomObject, InMemDicomObject};
 use dicom::{core::DataElement, dictionary_std::tags};
 
-use clap::{Args, Parser, Subcommand};
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(about)]
 struct Cli {
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Commands,
 }
 
 #[derive(Subcommand)]
@@ -25,12 +27,9 @@ fn main() {
     println!("HELLO");
     let args = Cli::parse();
     match &args.command {
-        Some(Commands::randomize { filename }) => {
+        Commands::randomize { filename } => {
             println!("Randomizing new dicom from file '{}'", filename);
             let _ = randomize_dicom(filename);
-        }
-        None => {
-            println!("No command selected");
         }
     }
 }
@@ -38,17 +37,30 @@ fn main() {
 fn randomize_dicom(input_file: &str) -> Result<(), Box<dyn std::error::Error>> {
     let mut obj = open_file(input_file).unwrap();
 
-    let new_patient_name = DataElement::new(
-        tags::PATIENT_NAME,
-        dicom::core::VR::PN,
-        dicom_value!(Str, "Andreas"),
-    );
-    obj.put(new_patient_name);
+    replace_tags(&mut obj);
 
-    let new_filename = format!("{}.random.dcm", input_file);
+    let uid = uuid::Uuid::new_v4();
+
+    let new_filename = format!("{}_{}.dcm", input_file.replace(".dcm", ""), uid.to_string());
     println!("Saving new random file '{}'", new_filename);
 
     obj.write_to_file(new_filename).unwrap();
 
     Ok(())
+}
+
+fn replace_tags(dicom: &mut FileDicomObject<InMemDicomObject>) {
+    let new_patient_name = DataElement::new(
+        tags::PATIENT_NAME,
+        dicom::core::VR::PN,
+        dicom_value!(Str, uuid::Uuid::new_v4().to_string()),
+    );
+    dicom.put(new_patient_name);
+
+    let new_patient_id = DataElement::new(
+        tags::PATIENT_ID,
+        dicom::core::VR::ST,
+        dicom_value!(Str, uuid::Uuid::new_v4().to_string()),
+    );
+    dicom.put(new_patient_id);
 }
