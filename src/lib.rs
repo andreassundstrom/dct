263bff886e1b7786b1745b4951a57f8cb025fb10
Andreas Sundström <andreas-sundstrom@hotmail.com>
2024-01-07 14:58:15 +0100

    Separate lib/main

---
+++ src/lib.rs
@@ -0,0 +1,100 @@
+use std::env::ArgsOs;
+use std::ffi::OsString;
+use std::io::BufWriter;
+
+use clap::{Parser, Subcommand};
+use dicom::dicom_value;
+use dicom::dump::{dump_file, dump_file_to};
+use dicom::object::{open_file, FileDicomObject, InMemDicomObject};
+use dicom::{core::DataElement, dictionary_std::tags};
+
+#[derive(Parser)]
+#[command(about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Display information about a dicom file
+    Dump {
+        #[arg(short, long)]
+        filename: String,
+    },
+    /// Create a new randomized dicom from a given input file
+    Randomize {
+        /// Filename for input
+        #[arg(short, long)]
+        filename: String,
+    },
+}
+
+pub fn dct_main<Item, Container: IntoIterator<Item = Item>>(args: Container)
+where
+    Item: Into<OsString> + std::clone::Clone,
+{
+    let args = Cli::parse_from(args);
+    match &args.command {
+        Commands::Randomize { filename } => {
+            println!("Randomizing new dicom from file '{}'", filename);
+            let _ = randomize_dicom(filename);
+        }
+        Commands::Dump { filename } => {
+            println!("Dumping file '{}'", filename);
+            let mut buffer = BufWriter::new(Vec::new());
+            dump_dicom_file(filename, &mut buffer);
+            let bytes = buffer.into_inner().unwrap();
+            let string = String::from_utf8(bytes).unwrap();
+            println!("{}", string);
+        }
+    }
+}
+
+fn dump_dicom_file(input_file: &str, buffer: &mut BufWriter<Vec<u8>>) {
+    let obj = match open_file(input_file) {
+        Ok(obj) => obj,
+        Err(err) => panic!("Failed to open file: {:?}", err),
+    };
+
+    dump_file(&obj).unwrap();
+    dump_file_to(buffer, &obj).unwrap();
+}
+
+fn randomize_dicom(input_file: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut obj = open_file(input_file).unwrap();
+
+    replace_tags(&mut obj);
+
+    let uid = uuid::Uuid::new_v4();
+    let path = std::path::Path::new(input_file);
+    let file_path = path.parent().unwrap().to_str().unwrap();
+    let old_filename = path.file_name().unwrap().to_str().unwrap();
+    let new_filename = format!(
+        "{}/{}_{}.dcm",
+        file_path,
+        old_filename.replace(".dcm", ""),
+        uid.to_string()
+    );
+    println!("Saving new random file '{}'", new_filename);
+
+    obj.write_to_file(new_filename).unwrap();
+
+    Ok(())
+}
+
+fn replace_tags(dicom: &mut FileDicomObject<InMemDicomObject>) {
+    let new_patient_name = DataElement::new(
+        tags::PATIENT_NAME,
+        dicom::core::VR::PN,
+        dicom_value!(Str, uuid::Uuid::new_v4().to_string()),
+    );
+    dicom.put(new_patient_name);
+
+    let new_patient_id = DataElement::new(
+        tags::PATIENT_ID,
+        dicom::core::VR::ST,
+        dicom_value!(Str, uuid::Uuid::new_v4().to_string()),
+    );
+    dicom.put(new_patient_id);
+}
